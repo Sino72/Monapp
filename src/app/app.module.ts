cb146cba33f6cd7b213b7c7096104a64293dd2ee
sino <sinocamino@yahoo.fr>
2019-09-18 10:13:42 +0200

    compilation ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,24 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent} from './product-details/product-details.Component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      {path: '/abc', component: ProductListComponent}]),
+      {path: '', component: ProductListComponent},
+    { path: 'products/:productId', component: ProductDetailsComponent }
+   ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+
+export class ProductDetailsComponent implements OnInit {
+
+    product;
+    constructor(
+        private route: ActivatedRoute,
+    ) { }
+    ngOnInit() {
+        // gyjtkr-
+    }
+}
+
+
+
